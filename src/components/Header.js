20f04cf2bbc660c92e4ea5a3dbea8a2c7d84bad0
Rahul Kumar <rahulsinghaniya1709@gmail.com>
2024-06-03 16:05:10 +0530

    TMDB API data fetch && built main container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 //import { FaUserAstronaut } from "react-icons/fa";
 import { auth } from '../utils/firebase';
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
+import { logo } from '../utils/constant';
 
 
 const Header = () => {
+
+    const dispatch = useDispatch();
+
     const navigate = useNavigate();
     const user = useSelector(store => store.user);
     const handleSignOut = () => {
         signOut(auth).then(() => {
             // Sign-out successful.
-            navigate("/");
         }).catch((error) => {
             // An error happened.
             navigate("/error");
         });
     }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+              const {uid, email, displayName, photoURL } = user;
+              dispatch(addUser({uid: uid, email: email,displayName: displayName, photoURL:photoURL} ));
+              navigate("/browse");
+            } else {
+              // User is signed out
+              dispatch(removeUser());
+              navigate("/");
+            }
+          });
+        return () => unsubscribe(); 
+    },[]);
+
     return (
         <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
             <img 
             className='w-44'
-            src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' 
+            src={logo}
             alt='logo' 
             />
            {user && ( <div className='flex'>
